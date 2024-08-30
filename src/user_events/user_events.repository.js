d2a225c227ed7ed2ff88e7c392478314bde9597a
Atma1 <atmasusanto08@gmail.com>
2024-08-30 19:42:26 +0800

    added insert user_event and update user_event status

--- src/user_events/user_events.repository.js
+++ src/user_events/user_events.repository.js
@@ -10,6 +10,29 @@ const getById = (user_events_id) =>
     },
   });
 
+const updateStatus = (user_event_id, updated_user_event_status) => (
+  prisma.user_events.update({
+    where: {
+      id: user_event_id
+    },
+    data: {
+      status: updated_user_event_status
+    }
+  })
+)
+
+const insert = (user_id, event_id) => (
+  prisma.user_events.create({
+    data: {
+      user_id: user_id,
+      event_id: event_id,
+      status: false,
+    }
+  })
+)
+
 module.exports = {
   getById,
+  insert,
+  updateStatus
 };
