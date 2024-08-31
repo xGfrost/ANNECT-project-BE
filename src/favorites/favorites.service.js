b6cd15f6338b1fd2eda1a60d691b2039854892a3
xGfrost <fadelakbar528@gmail.com>
2024-08-31 21:29:19 +0700

    removed duplicate import

--- src/favorites/favorites.service.js
+++ src/favorites/favorites.service.js
@@ -1,6 +1,5 @@
 const { getAll, deleteById, insert } = require("./favorites.repository");
 const { getbyid } = require("../users/users.service");
-const { getbyid } = require("../channels/channels.service");
 
 const getAllUserFavorites = async (user_id) => {
   await getbyid(user_id); // Check if user exists
