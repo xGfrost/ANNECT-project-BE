5009c0d09042032503db8ffa1762f217d6cecef4
Atma1 <atmasusanto08@gmail.com>
2024-08-31 12:52:21 +0800

    fixed wrong parameter

--- src/user_events/user_events.service.js
+++ src/user_events/user_events.service.js
@@ -2,7 +2,7 @@ const { getById, insert } = require("./user_events.repository");
 
 const getUserEventById = (user_id) => getById(user_id);
 
-const inserUserEvent = (user_id, event_id) => insert(user_id, event_id);
+const inserUserEvent = (user_event_data) => insert(user_event_data);
 
 module.exports = {
   getUserEventById,
