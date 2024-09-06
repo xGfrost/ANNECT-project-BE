e00a071ae7bbe14b58771438ec3ad11534af1fc3
xGfrost <fadelakbar528@gmail.com>
2024-09-06 21:07:00 +0700

    add is_favorite property to eventDetail

--- src/events/events.service.js
+++ src/events/events.service.js
@@ -20,8 +20,17 @@ const insertEvent = async (requestBody, eventData) => {
   return insert(eventData);
 };
 
-const getEventDetail = async (event_id) => {
+const getEventDetail = async (event_id, user_id) => {
   const eventDetail = await getDetail(event_id);
+
+    eventDetail.is_favorite = false;
+
+    
+    if (eventDetail.favorites && eventDetail.favorites.length > 0 && user_id != null) {
+        eventDetail.is_favorite = eventDetail.favorites.some((favorite) => favorite.user_id === user_id);
+    }
+
+
   const { tag_id, channel_id, id } = eventDetail;
   const similarEvent = await getSimilar(id, tag_id, channel_id);
   eventDetail.similar_event = similarEvent;
